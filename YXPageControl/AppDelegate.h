15b873eed620e611b85df689f4ded5d1b73d57d0
zdy <zdy@zdydemacbook-pro.local>
2016-06-07 13:26:39 +0800

    Initial Commit

---
+++ YXPageControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXPageControl
+//
+//  Created by zdy on 16/6/7.
+//  Copyright © 2016年 lianlian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXPageControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXPageControl
+//
+//  Created by zdy on 16/6/7.
+//  Copyright © 2016年 lianlian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
